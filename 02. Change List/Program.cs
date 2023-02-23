ec8579c0a22cc443899514d0ec7f0b543e73b3e7
cr0w <cr0w-@MILEN-PC>
2023-02-23 13:41:55 +0200

    sync

---
+++ 02. Change List/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToList();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] cmd = input.Split();
+                string currcmd = cmd[0];
+                if (currcmd == "Delete")
+                {
+                    int number = int.Parse(cmd[1]);
+                    num.RemoveAll(x => x == number);
+                }
+                else if (currcmd == "Insert")
+                {
+                    int number = int.Parse(cmd[1]);
+                    int index = int.Parse(cmd[2]);
+
+                    num.Insert(index, number);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", num));
+        }
+    }
+}
---
+++ 07. Append Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+        .Split("|", StringSplitOptions.RemoveEmptyEntries)
+        .ToList();
+
+            input.Reverse();
+            List<int> num = new List<int>();
+
+            foreach (var number in input)
+            {
+                num.AddRange(number.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList());
+            }
+
+            Console.WriteLine(string.Join(" ", num));
+        }
+    }
+}
